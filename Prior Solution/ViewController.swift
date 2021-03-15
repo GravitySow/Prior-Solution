b88471893284253accbfdb4dca79bef2560f6ee7
GravitySow <meen.ufo@gmail.com>
2021-03-15 18:18:57 +0700

    Initial Commit

---
+++ Prior Solution/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prior Solution
+//
+//  Created by มิตรภาพ สกุลพิสุทธิ์ on 15/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
